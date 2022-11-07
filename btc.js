1692b83d6dd0ab2c03a9e48ea9dcf1a7916ed7fa
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-07 22:19:22 +0100

    Gestion des erreurs

--- btc.js
+++ btc.js
@@ -2,11 +2,18 @@
 const axios = require('axios');
 
 async function main() {
-    const currency = process.argv[2].toUpperCase();
-    const response = await axios.get('http://api.coindesk.com/v1/bpi/currentprice.json');
-    const updatedAt = response.data.time.updated;
-    const rate = response.data.bpi[currency].rate;
-    console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
+    try {
+        const currency = process.argv[2].toUpperCase();
+        const response = await axios.get('http://api.coindesk.com/v1/bpi/currentprice.json');
+        if (!response.data.bpi[currency]) {
+            throw new Error('Unknown currency');
+        }
+        const updatedAt = response.data.time.updated;
+        const rate = response.data.bpi[currency].rate;
+        console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 main();
