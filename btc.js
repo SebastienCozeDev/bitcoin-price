c6f4ece8ad8b48ec800ea7e4593dc9299f6c91e1
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-07 22:33:58 +0100

    Refactoring du code

--- btc.js
+++ btc.js
@@ -2,17 +2,20 @@
 const axios = require('axios');
 
 async function main() {
+    /**
+     * Main function of this program.
+     */
+    const currency = process.argv[2]
+        ? process.argv[2].toUpperCase() 
+        : 'USD';
     try {
-        let currency = 'USD';
-        if (process.argv[2]) {
-            currency = process.argv[2].toUpperCase();
-        }
-        const response = await axios.get('http://api.coindesk.com/v1/bpi/currentprice.json');
-        if (!response.data.bpi[currency]) {
+        const url = 'http://api.coindesk.com/v1/bpi/currentprice.json';
+        const { data } = await axios.get(url);
+        if (!data.bpi[currency]) {
             throw new Error('Unknown currency');
         }
-        const updatedAt = response.data.time.updated;
-        const rate = response.data.bpi[currency].rate;
+        const updatedAt = data.time.updated;
+        const rate = data.bpi[currency].rate;
         console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
     } catch (err) {
         console.error(err.toString());
